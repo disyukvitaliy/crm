6582b1a89029b2289cf94dba36c3f5d3dde5aa71
disyukvitaliy <disyukvitaliy@gmail.com>
2016-01-28 20:29:19 +0600

    Added time_entries (migration, model, controller)

---
+++ app/controllers/time_entries_controller.rb
@@ -0,0 +1,73 @@
+class TimeEntriesController < ApplicationController
+  before_action :set_time_entry, only: [:show, :edit, :update, :destroy]
+  before_action :send_project_id_to_left_menu, :only => [:index, :show, :edit]
+
+  def all
+    @issues_grid = IssuesGrid.new(params[:issues_grid]) do |scope|
+      scope.page(params[:page]).per(5)
+    end
+    render :index
+  end
+
+  def index
+    @issues_grid = IssuesGrid.new(params[:issues_grid]) do
+      Project.find(params[:project_id]).issues.page(params[:page]).per(5)
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @issue = Issue.new
+  end
+
+  def edit
+  end
+
+  def create
+    @issue = Project.find(params[:project_id]).issues.new(issue_params)
+
+    if @issue.save
+      redirect_to @issue
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @issue.update(issue_params)
+      redirect_to @issue
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      format.js do
+        if @issue.destroy
+          render :js => "window.location = '#{request.referer}'"
+        else
+          render :alert, locals: {alert: @issue.errors.full_messages.join('\n')}
+        end
+      end
+      format.html do
+        if @issue.destroy
+          redirect_to project_issues_path(@issue.project_id)
+        else
+          render :edit
+        end
+      end
+    end
+  end
+
+  private
+  def set_time_entry
+    @time_entry = TimeEntry.find(params[:id])
+  end
+
+  def time_entry_params
+    params.require(:issue).permit(:subj, :descr, :start_date, :due_date, :issue_priority_id, :issue_status_id)
+  end
+end
